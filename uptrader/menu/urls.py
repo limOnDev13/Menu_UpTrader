b70b34ed1a203fbe2d59447e2c57451c5fe43051
Владимир Волосников <volosnikovvladimir68@gmail.com>
2025-05-13 22:55:09 +0500

    Add simple view func for testing tag draw_menu

--- uptrader/menu/urls.py
+++ uptrader/menu/urls.py
@@ -1,7 +1,11 @@
 """Module with urlpatterns."""
 
-from django.urls import include, path
+from django.urls import path
+
+from .views import test_draw_menu
 
 app_name = "menu"
 
-urlpatterns = []
+urlpatterns = [
+    path("test/", test_draw_menu, name="index"),
+]
--- uptrader/menu/views.py
+++ uptrader/menu/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def test_draw_menu(request: HttpRequest) -> HttpResponse:
+    return render(request, "menu/index.html", context={"target": "target"})
